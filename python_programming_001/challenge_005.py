e83a2ac238f749cfb6014231116fe5242eaf7f90
Achieng Mary <achieng75mary@gmail.com>
2023-08-29 19:07:12 +0300

    add two numbers then multiply the third with another number

---
+++ python_programming_001/challenge_005.py
@@ -0,0 +1,6 @@
+num1=int(input('enter a number: '))
+num2=int(input('enter a second number: '))
+num3=int(input('enter a third number: '))
+ans1=num1+num2
+answer=ans1*num3
+print('the answer is ', +answer)
