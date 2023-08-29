a25ceb619236ad590fce5c2bf96f0cc7e0bc8ad4
Achieng Mary <achieng75mary@gmail.com>
2023-08-29 19:11:57 +0300

    slices of pizza

---
+++ python_programming_001/challenge_006.py
@@ -0,0 +1,4 @@
+num1=int(input('How many slices of pizza did you start with? '))
+num2=int(input('How many slices of pizza have you eaten? '))
+answer=num1-num2
+print('You have ', +answer, 'slices of pizza left')
