a8fac10ab601fdd2b1cf48e32f533e2347f2ed20
Achieng Mary <achieng75mary@gmail.com>
2023-09-02 00:01:03 +0300

    division in python

--- python_programming_001/challenge_011.py
+++ python_programming_001/challenge_011.py
@@ -1,4 +1,4 @@
 num1=int(input('enter a number over 100: '))
-num2=int(imput('enter a number under 10: '))
+num2=int(input('enter a number under 10: '))
 ans=num1/num2
 print('the first number goes into the second number ', ans, 'times')
